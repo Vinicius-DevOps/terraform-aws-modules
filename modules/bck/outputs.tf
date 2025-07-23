d8c6ffb723be5ba07548b66bf8ade7ea8d1bd0fa
Vinicius Costa <costavinicius692@gmail.com>
2025-07-23 15:45:52 -0300

    Retira output do random_id

--- modules/bck/outputs.tf
+++ modules/bck/outputs.tf
@@ -6,7 +6,3 @@ output "daynamodb_table_name" {
   description = "Name of the DynamoDB table."
   value       = aws_dynamodb_table.main.name
 }
-output "random_id" {
-  description = "Random ID generated."
-  value = random_id.unique_id.hex
-}
